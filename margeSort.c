32fbc70448898d83703a543ab212c4605362fd90
MD MAHAMADUL HASAN SHAIKAT <113624647+mhshaikat95@users.noreply.github.com>
2023-10-03 15:06:12 +0000

    Update margeSort.c

--- margeSort.c
+++ margeSort.c
@@ -1,74 +1,81 @@
+
 #include <stdio.h>
 
-void printArray(int A[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
-}
-
-void marge(int A[], int mid, int low, int high)
-{
-    int i, j, k, B[100];
-    i = low;
-    j = mid + 1;
-    k = low;
-
-    while (i <= mid && j <= high)
-    {
-        if (A[i] < A[j])
-        {
-            B[k] = A[i];
+void marge(int arr[], int mid, int low, int high){
+    
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+    
+    int temp[100];
+    
+    while(i <= mid && j <= high){
+        
+        if(arr[i] < arr[j]){
+            
+            temp[k] = arr[i];
             i++;
             k++;
-        }
-        else
-        {
-            B[k] = A[j];
+            
+        }else{
+            temp[k] = arr[j];
             j++;
             k++;
+            
         }
     }
-    while (i <= mid)
-    {
-        B[k] = A[i];
-        k++;
+    
+    while(i <= mid ){
+        temp[k] = arr[i];
         i++;
-    }
-    while (j <= high)
-    {
-        B[k] = A[j];
         k++;
+    }
+    
+    while(j <= high){
+        temp[k] = arr[j];
         j++;
+        k++;
     }
-    for (int i = low; i <= high; i++)
-    {
-        A[i] = B[i];
+    
+    
+    for(int i = low; i <= high; i++){
+        arr[i] = temp[i];
     }
+    
     
 }
 
-     void margeSort(int A[], int low, int high){
-         int mid;
-         if(low<high){
-         mid = (low+high)/2;
-         margeSort(A, low, mid);
-         margeSort(A, mid+1, high);
-         marge(A, mid, low, high);
-         }
-     }
-     
+
+void margeSort(int arr[], int low, int high){
+    
+    int mid;
+    
+    if(low < high){
+        
+        mid = (low + high) / 2;
+        margeSort(arr, low, mid);
+        margeSort(arr, mid+1, high);
+        marge(arr, mid, low, high);
+        
+    }
+    
+    
+}
+
 
 int main()
 {
-    int A[] = {6,22,9,1,36,6,3,43,4,8};
-    int n = 10;
-    printf("Unsorted Array List: ");
-    printArray(A, n);
-    printf("Sorted Array List: ");
-    margeSort(A, 0, 6);
-    printArray(A, n);
+    
+    int arr[] = {21, 222, 111, 143, 233, 7, 5, 3, 1};
+    int n = 9;
+    
+    margeSort(arr, 0, 8);
+    
+    for(int i = 0; i < n; i++){
+        
+        printf("%d\t", arr[i]);
+    }
+    
     return 0;
 }
+
